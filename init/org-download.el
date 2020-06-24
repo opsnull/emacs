74daec6258d7bfca12062880cb6738b2d5ce9c62
张俊 <zhangjun@zhangjun-mbp.local>
2020-06-24 18:29:56 +0800

    update org-download

--- init/org-download.el
+++ init/org-download.el
@@ -6,13 +6,13 @@
   :ensure t
   :bind ("<f2>" . org-download-screenshot)
   :config
-  (require 'org-download)
   (setq org-download-method 'directory)
-  (setq org-download-image-dir "./images/")
+  (setq-default org-download-image-dir "./images/")
   (setq org-download-display-inline-images 'posframe)
   ;; org-download-screenshot-method "screencapture -i %s"
   (setq org-download-screenshot-method "pngpaste %s")
   (setq org-download-image-attr-list
         '("#+ATTR_HTML: :width 80% :align center"))
+  (add-hook 'dired-mode-hook 'org-download-enable)
   (org-download-enable)
   )
