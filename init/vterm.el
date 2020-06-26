b418cc1077aa8e899ce6e1feaf0e93549d72c90c
zhangjun <zhangjun@4paradigm.com>
2020-06-27 00:27:38 +0800

    update

--- init/vterm.el
+++ init/vterm.el
@@ -9,19 +9,6 @@
 
 ; 需要在 shell 的初始化文件（如 ~/.bashrc） 中一些 vterm_* 函数，具体参考：
 ; https://github.com/akermu/emacs-libvterm
-; 例如：
-;; function vterm_printf(){
-;;     if [ -n "$TMUX" ]; then
-;;         # Tell tmux to pass the escape sequences through
-;;         # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
-;;         printf "\ePtmux;\e\e]%s\007\e\\" "$1"
-;;     elif [ "${TERM%%-*}" = "screen" ]; then
-;;         # GNU screen (screen, screen-256color, screen-256color-bce)
-;;         printf "\eP\e]%s\007\e\\" "$1"
-;;     else
-;;         printf "\e]%s\e\\" "$1"
-;;     fi
-;; }
 
 ;; multi-vterm
 (use-package multi-vterm
