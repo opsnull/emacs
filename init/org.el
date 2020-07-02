78899cfb1cbaccce8babd6c832549476cd1192c4
zhangjun <zhangjun@4paradigm.com>
2020-07-02 11:14:39 +0800

    update org

--- init/org.el
+++ init/org.el
@@ -6,6 +6,7 @@
   (global-set-key (kbd "C-c l") 'org-store-link)
   (global-set-key (kbd "C-c a") 'org-agenda)
   (global-set-key (kbd "C-c c") 'org-capture)
+  (global-set-key (kbd "C-c b") 'org-switchb)
 )
 
 (use-package org-download
