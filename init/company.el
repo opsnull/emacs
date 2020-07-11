b635e4a9dfbb7a3d23800e1e5562da772e5279f5
张俊 <zhangjun@zhangjun-pc.local>
2020-07-11 19:57:38 +0800

    company-backends: 使用默认的 backends，但是将 capf 放在最前面。

--- init/company.el
+++ init/company.el
@@ -23,20 +23,18 @@
   (company-minimum-prefix-length 1)
   (company-tooltip-limit 14)
   (company-tooltip-align-annotations t)
-  ;; complete `abbrev' only in current buffer
-  (company-dabbrev-other-buffers nil)
-  ;; make dabbrev case-sensitive
+  (company-dabbrev-other-buffers t)
   (company-dabbrev-ignore-case nil)
   (company-dabbrev-downcase nil)
-  ;; make dabbrev-code case-sensitive
   (company-dabbrev-code-ignore-case nil)
   (company-dabbrev-code-everywhere t)
-  (company-backends '(company-capf
-                      company-files
-                      (company-dabbrev-code company-keywords)
-                      company-dabbrev))
   (company-global-modes '(not erc-mode message-mode help-mode gud-mode eshell-mode shell-mode))
   (company-frontends '(company-pseudo-tooltip-frontend company-echo-metadata-frontend))
+  ;; 重新定义 backends，将 cafp 放在最前面，防止无用的提示。
+  (company-backends  '(company-capf company-files company-bbdb
+                       company-eclim company-semantic company-clang company-xcode company-cmake
+                       (company-dabbrev-code company-gtags company-etags company-keywords)
+                       company-oddmuse company-dabbrev))
   :config
   (global-company-mode +1)
   )
