1d3bcfbf0ba10c99ab267c706ff463eaf359904b
张俊 <zhangjun@zhangjun-pc.local>
2020-07-11 21:05:45 +0800

    vterm: unmap c-l, don't clear screen.

--- init/vterm.el
+++ init/vterm.el
@@ -12,14 +12,18 @@
   ;; 而是使用 emacs 的 term-prompt-regexp 变量来匹配提示符。
   (setq vterm-use-vterm-prompt nil)
   (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] +")
-
+  :bind (:map vterm-mode-map
+              ("C-l" . nil)) ;; 不清理屏幕
   )
 
+; 支持多终端。
 (use-package multi-vterm
   :ensure t
   :after (vterm)
   :config
   (global-set-key [(control return)] 'multi-vterm)
+  (define-key vterm-mode-map (kbd "s-n")   'vterm-toggle-forward)
+  (define-key vterm-mode-map (kbd "s-p")   'vterm-toggle-backward)
   )
 
 (use-package vterm-toggle
@@ -29,8 +33,6 @@
   (global-set-key (kbd "C-`") 'vterm-toggle)
   (global-set-key (kbd "C-~") 'vterm-toggle-cd)
   (define-key vterm-mode-map [(control return)]   #'vterm-toggle-insert-cd)
-  (define-key vterm-mode-map (kbd "s-n")   'vterm-toggle-forward)
-  (define-key vterm-mode-map (kbd "s-p")   'vterm-toggle-backward)
   ; 在 frame 底部显示 term 窗口，https://github.com/jixiuf/vterm-toggle
   (setq vterm-toggle-fullscreen-p nil)
   (add-to-list 'display-buffer-alist
