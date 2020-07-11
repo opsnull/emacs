e03b72a6905256bf67725f2c2427c17782aacd35
opsnull <geekard@qq.com>
2020-07-11 21:09:47 +0800

    update

--- custom.el
+++ custom.el
@@ -1,1 +1,14 @@
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ox-hugo yasnippet yaml-mode wgrep-helm web-mode vterm-toggle use-package treemacs-projectile treemacs-magit treemacs-icons-dired tide terminal-focus-reporting smartparens restclient ox-reveal org-plus-contrib org-download ob-go names multi-vterm lsp-ui lsp-java json-mode js2-mode iedit htmlize helm-themes helm-rg helm-projectile helm-lsp helm-descbinds helm-ag goto-chg go-mode expand-region exec-path-from-shell doom-modeline dockerfile-mode diff-hl deadgrep dap-mode company-quickhelp color-theme-sanityinc-tomorrow)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
