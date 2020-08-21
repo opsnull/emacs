e329e2d73445e0d1b86d207e115b487422967355
opsnull <geekard@qq.com>
2020-08-21 13:55:28 +0800

    unmap vterm key.

--- init/vterm.el
+++ init/vterm.el
@@ -8,8 +8,12 @@
   (shell-command "which glibtool || brew install libtool")
   :config
   (setq vterm-max-scrollback 100000)
+  :bind
   ;; 不清理屏幕（会屏幕内容丢失）
-  :bind (:map vterm-mode-map ("C-l" . nil)))
+  (:map vterm-mode-map ("C-l" . nil))
+  ;; C-\ 被映射到 vterm-send-ctrl-slash，需要解绑后才能恢复以前的绑定（切换输入法）。
+  (:map vterm-mode-map ("C-\\" . nil))
+)
 
 ; 支持多终端。
 (use-package multi-vterm
@@ -24,6 +28,9 @@
 (use-package vterm-toggle
   :ensure t
   :after (vterm)
+  :custom
+  ;; vterm-toggle 打开终端时自动切换到 project root 目录。
+  (vterm-toggle-scope 'projectile)
   :config
   (global-set-key (kbd "C-`") 'vterm-toggle)
   (global-set-key (kbd "C-~") 'vterm-toggle-cd)
