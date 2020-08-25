16ba61da2ff3efdbde778767ac11b1dd70ad99a9
opsnull <geekard@qq.com>
2020-08-25 21:42:20 +0800

    update

--- init/vterm.el
+++ init/vterm.el
@@ -9,16 +9,17 @@
   :config
   (setq vterm-max-scrollback 100000)
   :bind
-  ;; 不清理屏幕（会屏幕内容丢失）
+  ;; 不清理屏幕（屏幕内容会丢失）。
   (:map vterm-mode-map ("C-l" . nil))
   ;; C-\ 被映射到 vterm-send-ctrl-slash，需要解绑后才能恢复以前的绑定（切换输入法）。
   (:map vterm-mode-map ("C-\\" . nil)))
 
-; 支持多终端。
+; 多终端。
 (use-package multi-vterm
   :ensure t
   :after (vterm)
   :config
+  ;; 重复执行，可以创建多个 vterm buffer。
   (global-set-key [(control return)] 'multi-vterm)
   (define-key vterm-mode-map (kbd "s-n") 'vterm-toggle-forward)
   (define-key vterm-mode-map (kbd "s-p") 'vterm-toggle-backward))
@@ -28,13 +29,13 @@
   :ensure t
   :after (vterm)
   :custom
-  ;; vterm-toggle 打开终端时自动切换到 project root 目录。
+  ;; 设置为 projectile scope 后，vterm-toggle 打开终端时自动切换到项目根目录。
+  ;; 如果要切换到 buffer 对应的目录，可以使用 vterm-toggle-cd 命令。
   (vterm-toggle-scope 'projectile)
   :config
   (global-set-key (kbd "C-`") 'vterm-toggle)
   (global-set-key (kbd "C-~") 'vterm-toggle-cd)
-  (define-key vterm-mode-map [(control return)] #'vterm-toggle-insert-cd)
-  ; 在 frame 底部显示 term 窗口，https://github.com/jixiuf/vterm-toggle
+  ;; 在 frame 底部显示终端窗口，https://github.com/jixiuf/vterm-toggle。
   (setq vterm-toggle-fullscreen-p nil)
   (add-to-list
    'display-buffer-alist
