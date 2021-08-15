62e07289a3162cc6cc4235ee05434f9e308a0061
opsnull <geekard@qq.com>
2021-08-15 22:55:47 +0800

    Import TRAMP performance and big refine.

--- early-init.el
+++ early-init.el
@@ -1,3 +1,23 @@
+(defconst sys/macp
+  (eq system-type 'darwin)
+  "Are we running on a Mac system?")
+
+(defconst sys/mac-x-p
+  (and (display-graphic-p) sys/macp)
+  "Are we running under X on a Mac system?")
+
+(defconst sys/mac-ns-p
+  (eq window-system 'ns)
+  "Are we running on a GNUstep or Macintosh Cocoa display?")
+
+(defconst sys/mac-cocoa-p
+  (featurep 'cocoa)
+  "Are we running with Cocoa on a Mac system?")
+
+(defconst sys/mac-port-p
+  (eq window-system 'mac)
+  "Are we running a macport build on a Mac system?")
+
 ;; Emacs 28
 (when (fboundp 'native-compile-async)
   (setenv "LIBRARY_PATH" 
@@ -11,6 +31,11 @@
 
 (setq package-native-compile t)
 
+(setq debug-on-error t)
+(add-hook 'after-init-hook
+          (lambda ()
+            (setq debug-on-error nil)))
+
 ;; Defer garbage collection further back in the startup process
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.5)
@@ -20,8 +45,11 @@
             (setq gc-cons-threshold 16777216 ;; 16MB
                   gc-cons-percentage 0.1)))
 
+;; Show garbage collections in minibuffer
+(setq garbage-collection-messages t)
+
 ;; Increase how much is read from processes in a single chunk (default is 4kb)
-(setq read-process-output-max #x10000)  ;; 64kb
+(setq read-process-output-max (* 1024 1024))  ;; 1mb
 
 ;; Don't ping things that look like domain names.
 (setq ffap-machine-p-known 'reject)
@@ -30,12 +58,13 @@
 (setq auto-mode-case-fold nil)  
 (setq idle-update-delay 1.0)
 
-;; Mac 平台下 Emacs native fullscreen 有 Bug，导致白屏和左右滑动问题，故使用传统
-;; 全屏模式。
+;; Mac 的 native fullscreen 会导致白屏和左右滑动问题，故使用传统全屏模式。
 (when (eq system-type 'darwin)
   (setq ns-use-native-fullscreen nil
         ns-use-fullscreen-animation nil))
-(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
 
-(add-hook 'after-init-hook #'toggle-frame-fullscreen)
-;;(add-hook 'after-init-hook #'toggle-frame-maximized)
+;;(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+;;(add-hook 'after-init-hook #'toggle-frame-fullscreen)
+
+(set-frame-parameter (selected-frame) 'maximized 'fullscreen)
+(add-hook 'after-init-hook #'toggle-frame-maximized)
