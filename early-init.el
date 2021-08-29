b428c321c396665c4de4116eba68cfcfd01a7bdb
opsnull <geekard@qq.com>
2021-08-29 23:13:00 +0800

    import performance and theme.

--- early-init.el
+++ early-init.el
@@ -1,22 +1,8 @@
-(defconst sys/macp
-  (eq system-type 'darwin)
-  "Are we running on a Mac system?")
-
-(defconst sys/mac-x-p
-  (and (display-graphic-p) sys/macp)
-  "Are we running under X on a Mac system?")
-
-(defconst sys/mac-ns-p
-  (eq window-system 'ns)
-  "Are we running on a GNUstep or Macintosh Cocoa display?")
-
-(defconst sys/mac-cocoa-p
-  (featurep 'cocoa)
-  "Are we running with Cocoa on a Mac system?")
-
-(defconst sys/mac-port-p
-  (eq window-system 'mac)
-  "Are we running a macport build on a Mac system?")
+(defconst sys/macp (eq system-type 'darwin) "Are we running on a Mac system?")
+(defconst sys/mac-x-p (and (display-graphic-p) sys/macp) "Are we running under X on a Mac system?")
+(defconst sys/mac-ns-p (eq window-system 'ns) "Are we running on a GNUstep or Macintosh Cocoa display?")
+(defconst sys/mac-cocoa-p (featurep 'cocoa) "Are we running with Cocoa on a Mac system?")
+(defconst sys/mac-port-p (eq window-system 'mac) "Are we running a macport build on a Mac system?")
 
 ;; Emacs 28
 (when (fboundp 'native-compile-async)
@@ -29,19 +15,10 @@
         native-comp-deferred-compilation-deny-list '()
         native-comp-async-report-warnings-errors 'silent))
 
-(setq package-native-compile t)
+(setq byte-compile-warnings '(cl-functions))
 
 (setq debug-on-error t)
 (add-hook 'after-init-hook (lambda () (setq debug-on-error nil)))
-
-;; Increase how much is read from processes in a single chunk (default is 4kb).
-(setq read-process-output-max (* 1024 1024))  ;; 1MB
-
-;; Don't ping things that look like domain names.
-(setq ffap-machine-p-known 'reject)
-
-;; Speed up startup
-(setq auto-mode-case-fold nil)  
 
 ;; Get rid of "For information about GNU Emacs..." message at startup, unless
 ;; we're in a daemon session where it'll say "Starting Emacs daemon." instead,
@@ -59,3 +36,6 @@
 
 (set-frame-parameter (selected-frame) 'maximized 'fullscreen)
 (add-hook 'after-init-hook #'toggle-frame-maximized)
+
+;; 使用单独文件保存自定义配置
+(setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
