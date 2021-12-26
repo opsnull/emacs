8656d87087a1aba4e9aff7987246b6a7e50ee50e
opsnull <geekard@qq.com>
2021-12-26 21:46:49 +0800

    add  org-journal.

---
+++ custom.el
@@ -0,0 +1,27 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(warning-suppress-types '((straight))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :foreground "red" :height 2.0))))
+ '(doom-modeline-buffer-file ((t (:inherit (mode-line bold)))))
+ '(org-block ((t (:font "Sarasa Term SC-14" :inherit fixed-pitch))))
+ '(org-block-begin-line ((t (:height 0.8 :underline "#A7A6AA"))))
+ '(org-block-end-line ((t (:height 0.8 :underline "#A7A6AA"))))
+ '(org-document-info ((t (:foreground "dark orange" :height 0.8))))
+ '(org-document-info-keyword ((t (:height 0.8))))
+ '(org-document-title ((t (:foreground "#ffb86c" :weight bold :height 1.5))))
+ '(org-drawer ((t (:height 0.8))))
+ '(org-link ((t (:foreground "royal blue" :underline t))))
+ '(org-meta-line ((t (:height 0.8))))
+ '(org-property-value ((t (:height 0.8))) t)
+ '(org-special-keyword ((t (:height 0.8))))
+ '(org-table ((t (:foreground "#83a598"))))
+ '(org-tag ((t (:weight bold :height 0.8))))
+ '(symbol-overlay-default-face ((t (:inherit (region bold))))))
