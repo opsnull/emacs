1dac9ad71fdac9f3406c8eda1b4f443b47df60cf
opsnull <geekard@qq.com>
2022-04-02 01:12:13 +0800

    refine: use modus-theme.

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5" default))
- '(warning-suppress-types '((straight) (straight) (straight))))
+ '(warning-suppress-types '((org-element-cache) (straight) (straight) (straight))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -13,16 +13,19 @@
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :foreground "red" :height 1.5))))
  '(doom-modeline-buffer-file ((t (:inherit (mode-line bold)))))
- '(org-block ((t (:font "Sarasa Term SC-15"))))
+ '(org-block ((t (:inherit 'fixed-pitch :height 0.9))))
  '(org-block-begin-line ((t (:height 0.8 :underline "#A7A6AA"))))
  '(org-block-end-line ((t (:height 0.8 :underline "#A7A6AA"))))
+ '(org-code ((t (:inherit 'fixed-pitch :height 0.9))))
  '(org-document-info ((t (:height 0.8))))
- '(org-document-info-keyword ((t (:height 0.8))))
+ '(org-document-info-keyword ((t (:inherit 'fixed-pitch :height 0.6))))
  '(org-document-title ((t (:foreground "#ffb86c" :weight bold :height 1.5))))
- '(org-drawer ((t (:height 0.8))))
+ '(org-drawer ((t (:inherit 'fixed-pitch :height 0.8))))
  '(org-link ((t (:foreground "royal blue" :underline t))))
- '(org-meta-line ((t (:height 0.8))))
+ '(org-meta-line ((t (:inherit 'fixed-pitch :height 0.7))))
  '(org-property-value ((t (:height 0.8))) t)
  '(org-special-keyword ((t (:height 0.8))))
+ '(org-table ((t (:inherit 'fixed-pitch :height 0.9))))
  '(org-tag ((t (:weight bold :height 0.8))))
+ '(org-verbatim ((t (:inherit 'fixed-pitch :height 0.9))))
  '(symbol-overlay-default-face ((t (:inherit (region bold))))))
